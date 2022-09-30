0a1d6ac758d0dcded3571c1373a32c6c7f593309
Victor Tarus <Tarus@NC-tarus-FVFC40T0L414.local>
2022-09-30 11:22:23 +0300

    Update : added unique hamburger child classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        double price = hamburger.itemizeHamburger();
+        hamburger.addHamburgerAddition1("tomato", 0.27);
+        hamburger.addHamburgerAddition2("lettuce", 0.27);
+        System.out.println("Total Burger price " + hamburger.itemizeHamburger());
+
+        HealthyBurger healthyBurger = new HealthyBurger("bacon" ,5.67);
+        healthyBurger.addHealthAddition2("Egg",5.43);
+        healthyBurger.itemizeHamburger();
     }
 }
